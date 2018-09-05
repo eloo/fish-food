085a308da1d4cd887a4116bf5b113cb9246d5fb6
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2018-09-05 22:01:35 +0200

    Update gitea.lua

--- Food/gitea.lua
+++ Food/gitea.lua
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-amd64",
-            sha256 = "84b91628501cb4b5c3cc32e2166f4f90ad6a9a34dd714038917aa1bfd6da0bc4",
+            --sha256 = "84b91628501cb4b5c3cc32e2166f4f90ad6a9a34dd714038917aa1bfd6da0bc4",
             resources = {
                 {
                     path = name .. "-" .. version .. "-linux-amd64",
