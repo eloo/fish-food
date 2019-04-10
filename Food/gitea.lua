1daa8a0ba8857e58c60f1774da7e06bf3ff540ee
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2019-04-10 20:57:39 +0200

    Update gitea to 1.7.5

--- Food/gitea.lua
+++ Food/gitea.lua
@@ -1,6 +1,6 @@
 local owner = "go-gitea"
 local name = "gitea"
-local version = "1.7.2"
+local version = "1.7.5"
 
 food = {
     name = name,
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-darwin-10.6-amd64",
-            sha256 = "20b0f2bc534d2a11b470e9fb81c607c832c5afdd71104e999d18519cc123d713",
+            sha256 = "dd4f3e90771aeb2f0a71262666c765c4d1c1da3bd45f03cc99e13827ed5e0243",
             resources = {
                 {
                     path = name .. "-" .. version .. "-darwin-10.6-amd64",
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-amd64",
-            sha256 = "b84098f0b0018071aa1ba5522078820494def29e6385c25c581c4362a4e463b3",
+            sha256 = "2afef775cb7acb56484630171dae4d0c0498d0a3f1a5e3f810deb7879fbe2ed4",
             resources = {
                 {
                     path = name .. "-" .. version .. "-linux-amd64",
@@ -39,7 +39,7 @@ food = {
             os = "linux",
             arch = "arm64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-arm64",
-            sha256 = "c6605f6b9b4933ebb3a6c6693bb963b4d37830ab263d1be3ad0e22d6f8eaed72",
+            sha256 = "01e5659f5e53365a9d3ff5126774065ff2bc9ef2f91253ae3f278a1ab1c0bed0",
             resources = {
                 {
                     path = name .. "-" .. version .. "-linux-arm64",
@@ -52,10 +52,10 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-windows-4.0-amd64.exe",
-            sha256 = "7ed51ff85e9ba88043307be8c97846aee812c412d755202cbb01231baed71a80",
+            sha256 = "d56e35c854f889b3e7d95e8d6063fdb9ce34165f41442e2a60524bc4c039070f",
             resources = {
                 {
-                    path = name .. "-" .. version .. "-windows-4.0-amd64.exe",
+                    path = name .. "-" .. version .. "-     ",
                     installpath = "bin\\" .. name .. ".exe"
                 }
             }
