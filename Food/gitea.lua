d0999ea7393842755d78bebe71f74d7954210d7c
Joseph Weigl <fents@gmx.net>
2019-08-15 22:31:40 +0200

    Update gitea

--- Food/gitea.lua
+++ Food/gitea.lua
@@ -1,6 +1,6 @@
 local owner = "go-gitea"
 local name = "gitea"
-local version = "1.8.3"
+local version = "1.9.1"
 
 food = {
     name = name,
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-darwin-10.6-amd64",
-            sha256 = "7125410814ad5b5e8d111a34ad4176091c4ba35618f3119b3f03f765ddfb37f3",
+            sha256 = "b1de3de8d00299bddbd01c5623014c9c2d50610457076953dc44d3e15f07e784",
             resources = {
                 {
                     path = name .. "-" .. version .. "-darwin-10.6-amd64",
@@ -26,7 +26,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-amd64",
-            sha256 = "7bb28b21cce4bdf0a24e6f6b21c064afa56d84904052dd55afdf59c419d49988",
+            sha256 = "2afe85726227e0c742ae73bb40fc49c61cf7e32b9992edb0b439a6e866699fb7",
             resources = {
                 {
                     path = name .. "-" .. version .. "-linux-amd64",
@@ -39,7 +39,7 @@ food = {
             os = "linux",
             arch = "arm64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-arm64",
-            sha256 = "e3569745122a793dbf1e86940a00c8843c0c3022513a9d9004593823b9e6abe1",
+            sha256 = "a2cf843271b98bb598d344214370dc4bfebeff0d9c081956fda87e557e6ec698",
             resources = {
                 {
                     path = name .. "-" .. version .. "-linux-arm64",
@@ -52,7 +52,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/" .. owner .. "/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-windows-4.0-amd64.exe",
-            sha256 = "d3701120311030df7090daebe369202233a02331a4ff5421d40d572c6265e50f",
+            sha256 = "ed93f373e91be41ba28f4a322daf9f906937c02041870591277c2e4d21031355",
             resources = {
                 {
                     path = name .. "-" .. version .. "-     ",
